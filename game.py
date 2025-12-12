1a254f0b246c30420d4a28f2829d22d5e0fc4881
Lozeth <finlayp97@gmail.com>
2025-12-13 02:20:19 +1100

    End of night

--- game.py
+++ game.py
@@ -1,2 +1,17 @@
 import random
+mylist=["Rock", "Paper", "Scissors"]
+Paper="paper"
+Scissors="scissors"
+Rock="rock"
 print("Let's play rock paper scissors!")
+print("3!")
+print("2!")
+print("1!")
+print("SHOOT")
+get_user_choice=input()
+get_computer_choice=(random.choice(mylist))
+get.winner()
+if winner==user print("I choose {get_computer_choice}, you win! Nice job!") else print("I chose {get_computer_choice}, I win!")
+print("Would you like to play again? Y/N")
+x=input()
+if x=Y restartscript else closewindow
