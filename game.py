e3140982692cfb65129ec549d5ff14d474300f73
Lozeth <finlayp97@gmail.com>
2025-12-14 17:29:51 +1100

    Small updates

--- game.py
+++ game.py
@@ -25,6 +25,13 @@ while x=="Y":
     if user_choice=="Scissors" and computer_choice=="Rock": winner=user_choice
     while user_choice==computer_choice:
         print("A draw! Let's go again")
+        print("3!")
+        time.sleep(1)
+        print("2!")
+        time.sleep(1)
+        print("1!")
+        time.sleep(1.5)
+        print("SHOOT")
         user_choice=input()
         computer_choice=(random.choice(mylist))
         if user_choice=="Paper" and computer_choice=="Scissors": winner=computer_choice
@@ -33,8 +40,9 @@ while x=="Y":
         if user_choice=="Rock" and computer_choice=="Paper": winner=user_choice
         if user_choice=="Scissors" and computer_choice=="Paper": winner=user_choice
         if user_choice=="Scissors" and computer_choice=="Rock": winner=user_choice
-    if winner==user_choice: print(f"I choose {computer_choice} you win! Nice job!")
+    if winner==user_choice: print(f"I choose {computer_choice}, you win!")
     if winner==computer_choice: print(f"I chose {computer_choice}, I win!")
+    time.sleep(1.5)
     print("Would you like to play again? Y/N")
     x=input()
 if x==("N"): print("Thanks joining me! Come back any time.") 
