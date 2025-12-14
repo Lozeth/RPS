7251d0796af1850278ff6d12dca9cf8905600bbf
Lozeth <finlayp97@gmail.com>
2025-12-14 16:54:24 +1100

    Game works?

--- game.py
+++ game.py
@@ -1,24 +1,41 @@
 import random
-import sleep from time
+import time
 mylist=["Rock", "Paper", "Scissors"]
 Paper="paper"
 Scissors="scissors"
 Rock="rock"
 print("Would you like to play a game? Y/N")
-a=input()
-print("Let's play rock paper scissors!")
-print("3!")
-sleep(1)
-print("2!")
-sleep(1)
-print("1!")
-sleep(1.5)
-print("SHOOT")
-get_user_choice=input()
-get_computer_choice=(random.choice(mylist))
-get.winner()
-#If get_user_choice=get_computer_choice print("A draw! Let's go again") then restartscriptfromline(9)
-if winner==user print("I choose {get_computer_choice}, you win! Nice job!") else print("I chose {get_computer_choice}, I win!")
-print("Would you like to play again? Y/N")
 x=input()
-if x=Y restartscriptfromline() else print("Thanks for playing! Come back any time.") then closewindow after sleep(5)
+while x=="Y":
+    print("Let's play rock paper scissors!")
+    print("3!")
+    time.sleep(1)
+    print("2!")
+    time.sleep(1)
+    print("1!")
+    time.sleep(1.5)
+    print("SHOOT")
+    user_choice=input()
+    computer_choice=(random.choice(mylist))
+    if user_choice=="Paper" and computer_choice=="Scissors": winner=computer_choice
+    if user_choice=="Paper" and computer_choice=="Rock": winner=user_choice
+    if user_choice=="Rock" and computer_choice=="Scissors": winner=user_choice
+    if user_choice=="Rock" and computer_choice=="Paper": winner=user_choice
+    if user_choice=="Scissors" and computer_choice=="Paper": winner=user_choice
+    if user_choice=="Scissors" and computer_choice=="Rock": winner=user_choice
+    while user_choice==computer_choice:
+        print("A draw! Let's go again")
+        user_choice=input()
+        computer_choice=(random.choice(mylist))
+        if user_choice=="Paper" and computer_choice=="Scissors": winner=computer_choice
+        if user_choice=="Paper" and computer_choice=="Rock": winner=user_choice
+        if user_choice=="Rock" and computer_choice=="Scissors": winner=user_choice
+        if user_choice=="Rock" and computer_choice=="Paper": winner=user_choice
+        if user_choice=="Scissors" and computer_choice=="Paper": winner=user_choice
+        if user_choice=="Scissors" and computer_choice=="Rock": winner=user_choice
+    if winner==user_choice: print(f"I choose {computer_choice} you win! Nice job!")
+    if winner==computer_choice: print(f"I chose {computer_choice}, I win!")
+    print("Would you like to play again? Y/N")
+    x=input()
+if x==("N"): print("Thanks joining me! Come back any time.") 
+time.sleep(5)
