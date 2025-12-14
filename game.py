bbe2041f72ce5589a6ef9739af1d71cbd59827be
Lozeth <finlayp97@gmail.com>
2025-12-14 15:21:12 +1100

    Mostly added goals + placeholders

--- game.py
+++ game.py
@@ -1,17 +1,24 @@
 import random
+import sleep from time
 mylist=["Rock", "Paper", "Scissors"]
 Paper="paper"
 Scissors="scissors"
 Rock="rock"
+print("Would you like to play a game? Y/N")
+a=input()
 print("Let's play rock paper scissors!")
 print("3!")
+sleep(1)
 print("2!")
+sleep(1)
 print("1!")
+sleep(1.5)
 print("SHOOT")
 get_user_choice=input()
 get_computer_choice=(random.choice(mylist))
 get.winner()
+#If get_user_choice=get_computer_choice print("A draw! Let's go again") then restartscriptfromline(9)
 if winner==user print("I choose {get_computer_choice}, you win! Nice job!") else print("I chose {get_computer_choice}, I win!")
 print("Would you like to play again? Y/N")
 x=input()
-if x=Y restartscript else closewindow
+if x=Y restartscriptfromline() else print("Thanks for playing! Come back any time.") then closewindow after sleep(5)
