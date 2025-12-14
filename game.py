769bd9554b506a6aac82feded5db0d30e4787686
Lozeth <finlayp97@gmail.com>
2025-12-14 17:48:48 +1100

    Fixes case issues

--- game.py
+++ game.py
@@ -1,12 +1,10 @@
 import random
 import time
-mylist=["Rock", "Paper", "Scissors"]
-Paper="paper"
-Scissors="scissors"
-Rock="rock"
+mylist=["rock", "paper", "scissors"]
 print("Would you like to play a game? Y/N")
 x=input()
-while x=="Y":
+y=x.casefold()
+while y=="y":
     print("Let's play rock paper scissors!")
     print("3!")
     time.sleep(1)
@@ -15,14 +13,15 @@ while x=="Y":
     print("1!")
     time.sleep(1.5)
     print("SHOOT")
-    user_choice=input()
+    z=input()
+    user_choice=z.casefold()
     computer_choice=(random.choice(mylist))
-    if user_choice=="Paper" and computer_choice=="Scissors": winner=computer_choice
-    if user_choice=="Paper" and computer_choice=="Rock": winner=user_choice
-    if user_choice=="Rock" and computer_choice=="Scissors": winner=user_choice
-    if user_choice=="Rock" and computer_choice=="Paper": winner=user_choice
-    if user_choice=="Scissors" and computer_choice=="Paper": winner=user_choice
-    if user_choice=="Scissors" and computer_choice=="Rock": winner=user_choice
+    if user_choice=="paper" and computer_choice=="scissors": winner=computer_choice
+    if user_choice=="paper" and computer_choice=="rock": winner=user_choice
+    if user_choice=="rock" and computer_choice=="scissors": winner=user_choice
+    if user_choice=="rock" and computer_choice=="paper": winner=user_choice
+    if user_choice=="scissors" and computer_choice=="paper": winner=user_choice
+    if user_choice=="scissors" and computer_choice=="rock": winner=user_choice
     while user_choice==computer_choice:
         print("A draw! Let's go again")
         print("3!")
@@ -34,16 +33,17 @@ while x=="Y":
         print("SHOOT")
         user_choice=input()
         computer_choice=(random.choice(mylist))
-        if user_choice=="Paper" and computer_choice=="Scissors": winner=computer_choice
-        if user_choice=="Paper" and computer_choice=="Rock": winner=user_choice
-        if user_choice=="Rock" and computer_choice=="Scissors": winner=user_choice
-        if user_choice=="Rock" and computer_choice=="Paper": winner=user_choice
-        if user_choice=="Scissors" and computer_choice=="Paper": winner=user_choice
-        if user_choice=="Scissors" and computer_choice=="Rock": winner=user_choice
+        if user_choice=="paper" and computer_choice=="scissors": winner=computer_choice
+        if user_choice=="paper" and computer_choice=="rock": winner=user_choice
+        if user_choice=="rock" and computer_choice=="scissors": winner=user_choice
+        if user_choice=="rock" and computer_choice=="paper": winner=user_choice
+        if user_choice=="scissors" and computer_choice=="paper": winner=user_choice
+        if user_choice=="scissors" and computer_choice=="rock": winner=user_choice
     if winner==user_choice: print(f"I choose {computer_choice}, you win!")
     if winner==computer_choice: print(f"I chose {computer_choice}, I win!")
     time.sleep(1.5)
     print("Would you like to play again? Y/N")
     x=input()
-if x==("N"): print("Thanks joining me! Come back any time.") 
+    y=x.casefold()
+if y==("n"): print("Thanks joining me! Come back any time.") 
 time.sleep(5)
